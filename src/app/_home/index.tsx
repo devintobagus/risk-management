ca099c946ebf05641592ab0e269b3b8e4bec3d01
devintobagus <dafabagus79@gmail.com>
2026-02-15 09:35:38 +0700

    fix: padding

--- src/app/_home/index.tsx
+++ src/app/_home/index.tsx
@@ -4,8 +4,8 @@ import { Stock } from "./_stock";
 
 export default function Home_() {
 	return (
-		<div className="min-h-screen flex justify-center items-center bg-muted p-4">
-			<div className="w-full max-w-xs">
+		<div className="min-h-screen flex justify-center items-center bg-muted">
+			<div className="w-full max-w-xs p-4">
 				<Carousel>
 					<CarouselContent >
 						<CarouselItem>
